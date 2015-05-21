541e2db3da63bdd12c6ddd209b21925c8859acdf
Steven Jackson <sanguine.steve@gmail.com>
2015-05-21 04:07:43 +0200

    Line Height - Accordion

---
+++ wp_custom_snippets/page selection.php
@@ -0,0 +1,8 @@
+//Used on http://www.roidigital.co.za/
+//Fix for previous developers work
+//Code was added within the CMS in the_content() function
+ 
+if (is_page(8994)){
+get_template_part ('page', 'thanks'); 
+}
+?>
